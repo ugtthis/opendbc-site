d55f97eae3d496f2504bbc34aa78a0a7bd34fd1d
ugtthis <dratunuman@gmail.com>
2025-08-11 22:26:57 -0400

    added fonts - some theme work

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,6 +9,12 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.svg" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin="anonymous" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=JetBrains+Mono:wght@100..800&display=swap"
+            rel="stylesheet"
+          />
           {assets}
         </head>
         <body>
