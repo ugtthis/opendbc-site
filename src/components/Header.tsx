efe4905fcca3f4606b3872d1429293ebfd1cabe0
ugtthis <dratunuman@gmail.com>
2025-08-12 03:29:42 -0400

    update format of styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,10 @@ const Header: Component = () => {
   return (
     <header
       role="banner"
-      class={`flex h-24 items-center justify-center bg-gradient-to-r from-[#132410] to-[#396c2f]
-        border-b-[3px] border-black shadow-[0_4px_3px_rgba(0,0,0,0.4)]`}
+      class={`
+        flex justify-center items-center h-24
+        bg-gradient-to-r border-black from-[#132410] to-[#396c2f] border-b-[3px] shadow-[0_4px_3px_rgba(0,0,0,0.4)]
+      `}
     >
       <A href="/">
         <img src="/opendbc-logo.png" alt="opendbc logo" class="h-auto w-[200px]" loading="eager" decoding="async" fetchpriority="high" />
